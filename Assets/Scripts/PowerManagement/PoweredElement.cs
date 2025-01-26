44cd41685ef79406989de294d7c6b5786d36d0e6
Jodi Foster <fosterjv111@gmail.com>
2025-01-26 12:10:26 -0500

    Power Management: zone and switch power logic moved from PoweredLight to PoweredElement
    since this logic would be re-used by anything that can be powered by a zone and optional physical switch

--- Assets/Scripts/PowerManagement/PoweredElement.cs
+++ Assets/Scripts/PowerManagement/PoweredElement.cs
@@ -4,21 +4,87 @@ using UnityEngine;
 
 /// <summary>
 /// Used as a wrapper for all types of things that can be powered and unpowered.
+/// Handles zone-level and switch-level power which all powered elements may use.
 /// </summary>
 public abstract class PoweredElement : MonoBehaviour
 {
-    /// <summary>
-    /// Called by the power zone to indicate that the zone is powered.
-    /// </summary>
-    public abstract void PowerUpZone();
+    [SerializeField, Tooltip("Index of the light switch corresponding with the PoweredLight. -1 if there is no switch.")]
+    protected int _switchIndex = -1;
+
+    private bool _isSwitchOn;
+    private bool _isZoneOn;
+
+    private void Awake()
+    {
+        // Precondition: must be an appropriate switch index in range of list (or -1)
+        if (_switchIndex < -1 || _switchIndex >= GameManager.Instance.SceneData.PowerSwitches.Length)
+            throw new System.Exception("Invalid Switch Index: MUST be either -1 or within the range of the Power Switches game manager list.");
+
+        // Determine starting switch state
+        _isSwitchOn = true; // on by default, unless overriden by switch state
+        if (_switchIndex != -1)
+        {
+            _isSwitchOn = GameManager.Instance.SceneData.PowerSwitches[_switchIndex];
+        }
+    }
 
     /// <summary>
     /// Called by the power zone to indicate that the zone is unpowered.
     /// </summary>
-    public abstract void PowerDownZone();
+    public void PowerDownZone()
+    {
+        _isZoneOn = false;
+        // light turns off no matter what
+
+        DisablePoweredElement();
+    }
+
+    /// <summary>
+    /// Called by the power zone to indicate that the zone is powered.
+    /// </summary>
+    public void PowerUpZone()
+    {
+        _isZoneOn = true;
+
+        if (_isSwitchOn) // only enable element if switch is ALSO on
+            EnablePoweredElement();
+    }
 
     /// <summary>
     /// Called by the power switch to switch the switch's toggle state.
     /// </summary>
-    public abstract void FlipPowerSwitch();
+    public void FlipPowerSwitch()
+    {
+        // Precondition: can only flip power switch if there is one that exists
+        if (_switchIndex == -1)
+            throw new System.Exception("Cannot call FlipPowerSwitch function! This PoweredLight has no corresponding switch.");
+
+        _isSwitchOn = !_isSwitchOn;
+
+        // turn on light if zone was already on
+        if (_isSwitchOn && _isZoneOn)
+            EnablePoweredElement();
+    }
+
+    /// <summary>
+    /// Returns whether the current element is powered or not.
+    /// Useful for darkness checks (for the creature).
+    /// </summary>
+    /// <returns></returns>
+    public bool IsPowered()
+    {
+        return _isSwitchOn && _isZoneOn;
+    }
+
+    /// <summary>
+    /// Toggles element as visually/functionally powered.
+    /// Does NOT handle checks for if it should be enabled.
+    /// </summary>
+    protected abstract void EnablePoweredElement();
+
+    /// <summary>
+    /// Toggles element as visually/functionally unpowered.
+    /// Does NOT handle checks for if it should be disabled.
+    /// </summary>
+    protected abstract void DisablePoweredElement();
 }
--- Assets/Scripts/PowerManagement/PoweredLight.cs
+++ Assets/Scripts/PowerManagement/PoweredLight.cs
@@ -3,57 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Handles functionality pertaining to a light element that can be toggled on/off in realtime.
+/// Handles implementation of functional light enabling and disabling.
 /// </summary>
 public class PoweredLight : PoweredElement
 {
-    [SerializeField, Tooltip("Index of the light switch corresponding with the PoweredLight. -1 if there is no switch.")]
-    private int SwitchIndex = -1;
     [SerializeField, Tooltip("Light component for enabling/disabling light source.")]
     private Light _light;
 
-    private bool _isSwitchOn;
-    private bool _isZoneOn;
-
-    private void Awake()
+    protected override void DisablePoweredElement()
     {
-        // Precondition: must be an appropriate switch index in range of list (or -1)
-        if (SwitchIndex < -1 || SwitchIndex >= GameManager.Instance.SceneData.PowerSwitches.Length)
-            throw new System.Exception("Invalid Switch Index: MUST be either -1 or within the range of the Power Switches game manager list.");
-
-        // Determine starting switch state
-        _isSwitchOn = true; // on by default, unless overriden by switch state
-        if (SwitchIndex != -1)
-        {
-            _isSwitchOn = GameManager.Instance.SceneData.PowerSwitches[SwitchIndex];
-        }
-    }
-
-    override public void PowerDownZone()
-    {
-        _isZoneOn = false;
-        // light turns off no matter what
         _light.enabled = false;
     }
 
-    override public void PowerUpZone()
+    protected override void EnablePoweredElement()
     {
-        _isZoneOn = true;
-
-        if (_isSwitchOn) // only enable light if switch is ALSO on
-            _light.enabled = true;
-    }
-
-    override public void FlipPowerSwitch()
-    {
-        // Precondition: can only flip power switch if there is one that exists
-        if (SwitchIndex == -1)
-            throw new System.Exception("Cannot call FlipPowerSwitch function! This PoweredLight has no corresponding switch.");
-
-        _isSwitchOn = !_isSwitchOn;
-
-        // turn on light if zone was already on
-        if (_isSwitchOn && _isZoneOn)
-            _light.enabled = true;
+        _light.enabled = true;
     }
 }
