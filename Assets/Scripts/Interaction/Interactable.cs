cf5d72a61150f3bafa68b235a51f9f5054586ae7
Andrew Kohler <awk480@gmail.com>
2025-02-03 17:47:28 -0500

    Interaction: Basic Components of Interaction
    You can look at something and hit E and it will be destroyed. True power.

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    // Placed on the interaction trigger of whatever the thing is
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void InteractEffects();
+}
--- Assets/Scripts/Interaction/Interactables/LogInteractable.cs
+++ Assets/Scripts/Interaction/Interactables/LogInteractable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactor : MonoBehaviour
+public class LogInteractable : Interactable
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class Interactor : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public override void InteractEffects()
+    {
+        Destroy(gameObject);
     }
 }
