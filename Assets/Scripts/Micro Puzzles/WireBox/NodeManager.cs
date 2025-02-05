811d633c7aaea8ed93a54166316f9bd21b676613
Jodi Foster <fosterjv111@gmail.com>
2025-02-05 14:37:35 -0500

    Wire Box: rendering wire between selected node and mouse pos on board
    only renders wire at all if mouse is over the peg board. Also clamps wire length based on the configured length of the selected wire type

--- Assets/Scripts/Micro Puzzles/WireBox/NodeManager.cs
+++ Assets/Scripts/Micro Puzzles/WireBox/NodeManager.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Handles proper processing of individual node clicks based on the bigger picture of selected wire and previous node states.
 /// </summary>
 public class NodeManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField, Tooltip("Used to access current selected wire state")]
     private WireManager _wireManager;
+    [SerializeField, Tooltip("Used to dynamically create, position, and scale wire connection")]
+    private GameObject _wireConnectionPrefab;
 
+    private InputAction _mousePosAction;
     private NodeSelector[] _nodes;
     private NodeSelector _firstNode = null; // null = none selected
+    private GameObject _currConnection = null;
 
     private void Awake()
     {
@@ -20,6 +26,8 @@ public class NodeManager : MonoBehaviour
         // Precondition: must contain nodes
         if (_nodes.Length == 0)
             throw new System.Exception("Incorrect Node Configuration. Node Manager must contain AT LEAST one NodeSelector");
+
+        _mousePosAction = InputSystem.actions.FindAction("MousePosition");
     }
 
     // Update is called once per frame
@@ -29,6 +37,66 @@ public class NodeManager : MonoBehaviour
         // Handles de-selecting node if wire is de-selected
         if (_wireManager.GetSelectedWire() is null)
             DeselectFirstNode();
+
+        // render wire between node and mouse
+        if (_firstNode is not null)
+        {
+            // create new connection
+            if (_currConnection is null)
+            {
+                // parented to node manager, positioned at node
+                _currConnection = Instantiate(_wireConnectionPrefab, _firstNode.transform.position, _wireConnectionPrefab.transform.rotation, transform);
+            }
+
+            // update connection
+            Vector3 mousePos = _mousePosAction.ReadValue<Vector2>();
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            if (Physics.Raycast(ray, out RaycastHit hit, 5f, LayerMask.GetMask("WireBoxGrid")))
+            {
+                // show wire (mouse is on grid)
+                _currConnection.SetActive(true);
+
+                // fetch endpoints
+                Vector3 hitPos = hit.point;
+                hitPos.z = _currConnection.transform.position.z;
+                Vector3 nodePos = _firstNode.transform.position;
+
+                // Max Length
+                float maxLength = _wireManager.GetSelectedWire().Length;
+                if (Vector3.Distance(hitPos, nodePos) > maxLength / 10f) // 10x scale factor from unity coords to length unit
+                {
+                    // POSITION
+                    Vector3 newEndpoint = nodePos + ((hitPos - nodePos).normalized * maxLength / 10f); // 10x scale factor from unity coords to length unit
+                    _currConnection.transform.position = (nodePos + newEndpoint) / 2f;
+
+                    // SCALE
+                    Vector3 scale = _currConnection.transform.localScale;
+                    scale.x = _wireManager.GetSelectedWire().Length;
+                    _currConnection.transform.localScale = scale;
+                }
+                // Within Length Limit
+                else
+                {
+                    // POSITION
+                    _currConnection.transform.position = (hitPos + nodePos) / 2f;
+
+                    // SCALE
+                    Vector3 scale = _currConnection.transform.localScale;
+                    scale.x = Vector3.Distance(hitPos, nodePos) * 10f; // 10x scale factor from unity coords to grid units
+                    _currConnection.transform.localScale = scale;
+                }
+
+                // ROTATE
+                Quaternion rot = Quaternion.LookRotation(nodePos - hitPos, Vector3.forward);
+                rot *= Quaternion.Euler(0, 90, 0); // rotate 90 degrees (to account for improperly aligned forward)
+                _currConnection.transform.rotation = rot;
+            }
+            else
+            {
+                // don't show wire (mouse is not on grid)
+                _currConnection.SetActive(false);
+            }
+        }
     }
 
     public void ProcessNodeClick(NodeSelector node)
