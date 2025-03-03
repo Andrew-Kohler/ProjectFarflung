fba4076a54c4d7465d186bb43bb84c9c751ac0e1
Jodi Foster <fosterjv111@gmail.com>
2025-03-03 13:12:57 -0500

    Controls Remapping: reworked move inputs to no longer be composites
    rebinding doesn't like composites.
    
    required some jerry-rigging of the input class from the StarterAssets player controller

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -22,8 +22,63 @@ namespace StarterAssets
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
 
+		// move input actions
+		private InputAction _moveForward;
+		private InputAction _moveRight;
+		private InputAction _moveBackward;
+		private InputAction _moveLeft;
+
+		private void OnEnable()
+        {
+			// bind input updating
+			_moveForward = InputSystem.actions.FindAction("MoveForward");
+			_moveForward.started += UpdateMoveInput;
+			_moveForward.canceled += UpdateMoveInput;
+			_moveRight = InputSystem.actions.FindAction("MoveRight");
+			_moveRight.started += UpdateMoveInput;
+			_moveRight.canceled += UpdateMoveInput;
+			_moveBackward = InputSystem.actions.FindAction("MoveBackward");
+			_moveBackward.started += UpdateMoveInput;
+			_moveBackward.canceled += UpdateMoveInput;
+			_moveLeft = InputSystem.actions.FindAction("MoveLeft");
+			_moveLeft.started += UpdateMoveInput;
+			_moveLeft.canceled += UpdateMoveInput;
+		}
+        private void OnDisable()
+        {
+			// unbind input updating
+			_moveForward.started += UpdateMoveInput;
+			_moveForward.canceled += UpdateMoveInput;
+			_moveRight.started += UpdateMoveInput;
+			_moveRight.canceled += UpdateMoveInput;
+			_moveBackward.started += UpdateMoveInput;
+			_moveBackward.canceled += UpdateMoveInput;
+			_moveLeft.started += UpdateMoveInput;
+			_moveLeft.canceled += UpdateMoveInput;
+		}
+
+        private void UpdateMoveInput(InputAction.CallbackContext context)
+        {
+            // fetch inputs for move every frame
+            // cannot use OnMove below since we are no longer using a composite
+            int xInput = 0;
+			int yInput = 0;
+			if (_moveRight.ReadValue<float>() > 0.5f)
+                xInput++;
+			if (_moveLeft.ReadValue<float>() > 0.5f)
+				xInput--;
+			if (_moveForward.ReadValue<float>() > 0.5f)
+				yInput++;
+			if (_moveBackward.ReadValue<float>() > 0.5f)
+				yInput--;
+
+			// update the value actually used in the player controller
+			MoveInput(new Vector2(xInput, yInput));
+        }
+
+
 #if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
+        public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
@@ -56,8 +111,7 @@ namespace StarterAssets
 			TabInput(value.isPressed);
 		}
 #endif
-
-
+		
 		public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
