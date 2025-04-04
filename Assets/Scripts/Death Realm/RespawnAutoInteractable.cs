8f6cead9015d053b1d5fc82b01c7f4fa63490933
Jodi Foster <fosterjv111@gmail.com>
2025-04-04 17:07:24 -0400

    Death Realm: camera movement now moves up and then dives into player mask

--- Assets/Scripts/Death Realm/RespawnAutoInteractable.cs
+++ Assets/Scripts/Death Realm/RespawnAutoInteractable.cs
@@ -9,8 +9,10 @@ using Cinemachine;
 public class RespawnAutoInteractable : MonoBehaviour
 {
     [Header("Animation Sequence")]
+    [SerializeField, Tooltip("The camera aimed down on the player from above")]
+    private CinemachineVirtualCamera _visorCam1;
     [SerializeField, Tooltip("The camera aimed into the visor")]
-    private CinemachineVirtualCamera _visorCam;
+    private CinemachineVirtualCamera _visorCam2;
     [SerializeField, Tooltip("Handles fade to black and vitals display animations")]
     private Animator _anim;
 
@@ -27,12 +29,18 @@ public class RespawnAutoInteractable : MonoBehaviour
         // pan camera into visor
         CinemachineVirtualCamera mainCam = Camera.main.GetComponent<CinemachineBrain>().
             ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>();
-        _visorCam.gameObject.SetActive(true);
+        _visorCam1.gameObject.SetActive(true);
         mainCam.gameObject.SetActive(false);
+        
+        // wait for camera to be for sure above the player
+        yield return new WaitForSeconds(2f);
 
+        _visorCam1.gameObject.SetActive(false);
+        _visorCam2.gameObject.SetActive(true);
+        
         // wait for camera to be for sure within the visor
-        yield return new WaitForSeconds(2.0f);
-
+        yield return new WaitForSeconds(1.5f);
+        
         // activate respawn animator
         _anim.SetTrigger("Activate");
 
