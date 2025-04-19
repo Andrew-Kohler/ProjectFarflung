a33a18337d2fd4bd65ebe7271bcc7a8dd7474990
Jodi Foster <fosterjv111@gmail.com>
2025-04-19 14:40:15 -0400

    Spatial Audio: no longer plays at wrong volume level during scene enter transition

--- Assets/Scripts/Audio/AudioSourceVolumeSetter.cs
+++ Assets/Scripts/Audio/AudioSourceVolumeSetter.cs
@@ -11,17 +11,40 @@ public class AudioSourceVolumeSetter : MonoBehaviour
     [SerializeField, Tooltip("Used to control volume level of audio source.")]
     private AudioSource _audioSource;
 
+    private bool _isPaused = false;
+
     private void Awake()
     {
-        // 25% volume while paused, otherwise 100%
-        // necessary to ensure that spatial audio is properly reduced by sliders even during scene enter transition
-        _audioSource.volume = GameManager.GetSFXVolume(); // cannot be paused during scene transition
+        // ensure volume can be changed, even during scene enter when timescale is 0
+        _audioSource.velocityUpdateMode = AudioVelocityUpdateMode.Dynamic;
     }
 
     // Update is called once per frame
     void Update()
     {
         // 25% volume while paused, otherwise 100%
-        _audioSource.volume = (Time.timeScale == 0 ? 0.25f : 1f) * GameManager.GetSFXVolume();
+        _audioSource.volume = (_isPaused ? 0.25f : 1f) * GameManager.GetSFXVolume();
+    }
+
+    private void OnEnable()
+    {
+        PauseControls.onPauseOpen += SetPause;
+        PauseControls.onPauseClose += SetUnpause;
+    }
+
+    private void OnDisable()
+    {
+        PauseControls.onPauseOpen -= SetPause;
+        PauseControls.onPauseClose -= SetUnpause;
+    }
+
+    private void SetPause()
+    {
+        _isPaused = true;
+    }
+
+    private void SetUnpause()
+    {
+        _isPaused = false;
     }
 }
