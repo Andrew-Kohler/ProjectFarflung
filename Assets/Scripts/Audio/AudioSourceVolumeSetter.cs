34093eee33b0e97c04bff364ad5fbe8916f8778b
Jodi Foster <fosterjv111@gmail.com>
2025-04-19 13:32:36 -0400

    Spatial Audio: spatial audio sources now have volume reduced to match saved slider value even during scene enter transition

--- Assets/Scripts/Audio/AudioSourceVolumeSetter.cs
+++ Assets/Scripts/Audio/AudioSourceVolumeSetter.cs
@@ -11,6 +11,13 @@ public class AudioSourceVolumeSetter : MonoBehaviour
     [SerializeField, Tooltip("Used to control volume level of audio source.")]
     private AudioSource _audioSource;
 
+    private void Awake()
+    {
+        // 25% volume while paused, otherwise 100%
+        // necessary to ensure that spatial audio is properly reduced by sliders even during scene enter transition
+        _audioSource.volume = GameManager.GetSFXVolume(); // cannot be paused during scene transition
+    }
+
     // Update is called once per frame
     void Update()
     {
