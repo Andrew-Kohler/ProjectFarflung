15974ba8592ef2ae7439d6caac11aa721ed5d63a
Andrew Kohler <awk480@gmail.com>
2025-04-18 22:51:30 -0400

    Pause and options: Added assets to Quit button popup

--- Assets/Scripts/HUD/ControlsTextHUD.cs
+++ Assets/Scripts/HUD/ControlsTextHUD.cs
@@ -56,6 +56,12 @@ public class ControlsTextHUD : MonoBehaviour
                 case 6:
                     _text.text = "SUIT DISPLAY MINIMIZED * PRESS " + action.ToUpper() + " TO RESTORE DISPLAY";
                     break;
+                case 7:
+                    _text.text = "Press " + action.ToUpper() + " to toggle";
+                    break;
+                case 8:
+                    _text.text = "DANGER: Hold " + action.ToUpper() + " to overclock";
+                    break;
                 default:
                     _text.text = "Wuh oh";
                     break;
