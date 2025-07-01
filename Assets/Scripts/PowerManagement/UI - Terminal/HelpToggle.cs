31defcfdabbb286750b206b135b31f9677b7a018
Jodi Foster <fosterjv111@gmail.com>
2025-07-01 13:31:08 -0400

    Terminal: functional navigation between standard terminal interface and help menu through button in top left corner

---
+++ Assets/Scripts/PowerManagement/UI - Terminal/HelpToggle.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the showing, hiding, and animating of the terminal help menu through a button.
+/// </summary>
+public class HelpToggle : MonoBehaviour
+{
+    [SerializeField, Tooltip("Used to smoothly animate help menu in and out.")]
+    private CanvasGroup _alphaGroup;
+    [SerializeField, Tooltip("Rate at which fade in/out occurs.")]
+    private float _fadeRate;
+
+    private bool _isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // configure menu closed by default
+        _alphaGroup.alpha = 0;
+        _isOpen = false;
+        _alphaGroup.blocksRaycasts = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isOpen)
+        {
+            _alphaGroup.alpha += _fadeRate * Time.deltaTime;
+
+            // start blocking raycasts as soon as it starts opening
+            _alphaGroup.blocksRaycasts = true;
+        }
+        else
+        {
+            _alphaGroup.alpha -= _fadeRate * Time.deltaTime;
+
+            // stop blocking raycasts only once fully closed
+            if (_alphaGroup.alpha <= 0)
+                _alphaGroup.blocksRaycasts = false;
+        }
+    }
+
+    public void ToggleHelpMenu()
+    {
+        _isOpen = !_isOpen;
+
+        // terminal press SFX
+        AudioManager.Instance.PlayTerminalArrow();
+    }
+}
